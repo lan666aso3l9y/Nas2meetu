fbb4a34d440b16098bd70e0ab23f1f191e19500b
lan666aso3l9y <gongbai64612@163.com>
2022-03-24 14:00:44 +0800

    subido programa

--- Programa/src/is/hotelzargo/integracion/exception/IntegrationException.java
+++
@@ -1,9 +0,0 @@
-package is.hotelzargo.integracion.exception;
-
-@SuppressWarnings("serial")
-public class IntegrationException extends Exception {
-
-	public IntegrationException(String err){
-		super(err);
-	}
-}
--- Programa/src/is/hotelzargo/negocio/exception/BusinessException.java
+++
@@ -1,9 +0,0 @@
-package is.hotelzargo.negocio.exception;
-
-@SuppressWarnings("serial")
-public class BusinessException extends Exception {
-
-	public BusinessException(String err){
-		super(err);
-	}
-}
