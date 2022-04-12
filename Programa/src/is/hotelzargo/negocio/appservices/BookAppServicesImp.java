c97121d23fdad77ef9b52837dac28831d67bc10e
lan666aso3l9y <gongbai64612@163.com>
2022-04-13 00:26:39 +0800

    borrado del programa

--- Programa/src/is/hotelzargo/negocio/appservices/BookAppServicesImp.java
+++
@@ -1,5 +0,0 @@
-package is.hotelzargo.negocio.appservices;
-
-public class BookAppServicesImp implements BookAppServices {
-
-}
--- Programa/src/is/hotelzargo/negocio/appservices/ServicesAppServicesImp.java
+++
@@ -1,5 +0,0 @@
-package is.hotelzargo.negocio.appservices;
-
-public class ServicesAppServicesImp implements ServicesAppServices {
-
-}
