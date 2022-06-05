ef2e2437d9846103244ff8d277e93a4a46b25ccb
lan666aso3l9y <gongbai64612@163.com>
2022-06-06 04:26:43 +0800

    recolocación de archivos, que me había equivocado al organizarlos. ShiftTransfer.java  moverlo a la carpeta de transfer

--- Programa/src/is/hotelzargo/negocio/transfer/ShiftTransfer.java
+++ Programa/src/is/hotelzargo/negocio/transfer/ShiftTransfer.java
@@ -1,8 +1,54 @@
 package is.hotelzargo.negocio.transfer;
 
 public class ShiftTransfer {
+	//turno, horaentrada, horasalida, id
+	private string turno;
+	private date horaEntrada;
+	private date horaSalida;
+	private int id;
 	
-	public ShiftTransfer(){
+	
+	public ShiftTransfer(string turno,
+						date horaEntrada,
+						date horaSalida,
+						int id){
 		//TODO transfer turnos
+		this.turno = turno;
+		this.horaEntrada = horaEntrada;
+		this.horaSalida = horaSalida;
+		this.id;
+	}
+	
+	public String getTurno () {
+		return turno;
+	}
+	
+	public date getHoraEntrada () {
+		return horaEntrada;
+	}
+	
+	public date getHoraSalida () {
+		return horaSalida;
+	}
+	
+	public int getId () {
+		return id;
+	}
+	
+	
+	public void setTurno (string turno) {
+		this.turno = turno;
+	}
+	
+	public void setHoraEntrada (date horaEntrada) {
+		this.horaEntrada = horaEntrada;
+	}
+	
+	public void setHoraSalida (date horaSalida) {
+		this.horaSalida = horaSalida;
+	}
+	
+	public void setId (int id) {
+		this-id = id;
 	}
 }
