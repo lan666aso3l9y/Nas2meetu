188632f9c2d9f5e3b9c03b4d8f2288583ea788e3
lan666aso3l9y <gongbai64612@163.com>
2022-06-13 01:36:05 +0800

    creado EmployeeTransfers todos los get y set

--- Programa/src/is/hotelzargo/negocio/transfer/EmployeeTransfer.java
+++ Programa/src/is/hotelzargo/negocio/transfer/EmployeeTransfer.java
@@ -1,5 +1,84 @@
 package is.hotelzargo.negocio.transfer;
 
+import is.hotelzargo.negocio.appservices.ShiftAppServices;
+
 public class EmployeeTransfer {
 
+	private int id;
+	private ShiftAppServices shift;
+	private float pay;
+	private String name;
+	private String surname;
+	private String DNI;
+	private int tlf;
+	
+	public void EmployeeTransfers (int id, 
+							  ShiftAppServices shift, 
+							  float pay,
+							  String name,
+							  String surname,
+							  String DNI,
+							  int tlf){
+		
+		this.id = id;
+		this.shift = shift;
+		this.pay = pay;
+		this.name = name;
+		this.surname = surname;
+		this.DNI = DNI;
+		this.tlf = tlf;
+		
+	}
+	
+	public int getId(){
+		return this.id;
+	}
+	
+	public ShiftAppServices getShift(){
+		return this.shift;
+	}
+	
+	public float getPay(){
+		return this.pay;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public String getSurname(){
+		return this.surname;
+	}
+	
+	public String getDNI(){
+		return this.DNI;
+	}
+	
+	public int getTlf(){
+		return this.tlf;
+	}
+	
+	public void setShift(ShiftAppServices shift){
+		this.shift = shift;
+	}
+	
+	public void setPay(float pay){
+		this.pay = pay;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setSurname(String surname){
+		this.surname = surname;
+	}
+	
+	public void setDNI(String DNI){
+		this.DNI = DNI;
+	}
+	
+	public void setTlf(int tlf){
+		this.tlf = tlf;
+	}
 }
