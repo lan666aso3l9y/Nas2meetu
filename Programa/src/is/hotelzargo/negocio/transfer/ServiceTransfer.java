cbf2080ccffc9c6a004263059f64c1e2618a6773
lan666aso3l9y <gongbai64612@163.com>
2022-06-28 09:54:54 +0800

    transfer services creado

--- Programa/src/is/hotelzargo/negocio/transfer/ServiceTransfer.java
+++ Programa/src/is/hotelzargo/negocio/transfer/ServiceTransfer.java
@@ -1,5 +1,39 @@
 package is.hotelzargo.negocio.transfer;
 
 public class ServiceTransfer {
+	
+	private int iD;
+	private String services;
+	
+	/*
+	 * getters
+	 */
+	public int getId(){
+		return this.iD;
+	}
+	
+	public String getServices(){
+		return this.services;
+	}
+	
+	/*
+	 *  setters
+	 */
+	public void setId(int iD){
+		
+		this.iD = iD;
+	}
+	
+	public void setServices(String services){
+		this.services = services;
+	}
+	
+	/*
+	 * Add new Services in a Service that already exists
+	 */
+	public void addServices(String services){
+		this.services += services;	
+	}
+	
 
 }
