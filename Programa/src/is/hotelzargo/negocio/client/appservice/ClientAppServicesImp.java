c7ae045f9881ea94f2e1e21f2877062c0679a365
lan666aso3l9y <gongbai64612@163.com>
2022-12-16 08:39:49 +0800

    limpiando appservice CLIENT

--- Programa/src/is/hotelzargo/negocio/client/appservice/ClientAppServicesImp.java
+++ Programa/src/is/hotelzargo/negocio/client/appservice/ClientAppServicesImp.java
@@ -23,7 +23,6 @@ public class ClientAppServicesImp implements ClientAppServices {
 			checkDataIndividual(t);
 			
 			try {
-				//Se busca antes de insertar en base de datos porque mysql
 				if(!dao.searchClient(((ClientTransfer) t).getID())){
 					dao.createClientIndividual(t);
 				}else{
@@ -50,22 +49,87 @@ public class ClientAppServicesImp implements ClientAppServices {
 		}
  
 	}
+
+	@Override
+	public void delClient(int id) throws ClientAppServicesException {
+		DAOFactory fac = DAOFactory.getInstance();
+		ClientDAO dao = fac.getClientDAO();
+		
+		try {
+			if (dao.searchClient(id)){
+				if(dao.allBooksConfirmed(id)){
+					dao.deleteClient(id);
+				}
+				else{
+					throw new ClientAppServicesException("El usuario a eliminar tiene reservas pendientes de confirmar");
+				}
+			}else{
+				throw new ClientAppServicesException("El usuario a eliminar no existe");
+			}
+		} catch (ClientIntegrationException e) {
+			throw new ClientAppServicesException(e.getMessage());
+		}
+		
+	}
+
+	@Override
+	public Vector<ClientTransfer> listClient() throws ClientAppServicesException {
+		DAOFactory fac = DAOFactory.getInstance();
+		ClientDAO dao = fac.getClientDAO();
+		try {
+			return dao.listClient();
+		} catch (ClientIntegrationException e) {
+			throw new ClientAppServicesException(e.getMessage());
+		}
+
+	}
+
+	@Override
+	public void modClient(ClientTransfer t) throws ClientAppServicesException {
+
+		DAOFactory fac = DAOFactory.getInstance();
+		ClientDAO dao = fac.getClientDAO();
+		
+		if(t instanceof ClientTransferIndividual ) {
+			try {
+				if(dao.searchClient(t.getID())){
+					dao.updateClientIndividual(t);
+				}else{
+					throw new ClientAppServicesException("Cliente no encontrado");
+				}
+			} catch (ClientIntegrationException e) {
+				throw new ClientAppServicesException(e.getMessage());
+			}
+		}
+		else{
+			try {
+				if(dao.searchClient(t.getID())){
+					dao.updateClientCompany(t);
+				}else{
+					throw new ClientAppServicesException("Cliente no encontrado");
+				}
+			} catch (ClientIntegrationException e) {
+				throw new ClientAppServicesException(e.getMessage());
+			}
+		}
+		
+	}
 	
 	private void checkDataIndividual(ClientTransfer t) throws ClientAppServicesException {
 		
-		String DNI = ((ClientTransferIndividual)t).getDNI();
-		String TLF = ((ClientTransferIndividual)t).getPhone();
-		String TJC = ((ClientTransferIndividual)t).getCreditCard();
+		String dni = ((ClientTransferIndividual)t).getDNI();
+		String phone = ((ClientTransferIndividual)t).getPhone();
+		String creditCard = ((ClientTransferIndividual)t).getCreditCard();
 		
 		if(((ClientTransferIndividual)t).getName().length() < 3)
 			throw new ClientAppServicesException("Nombre no valido");
 		if(((ClientTransferIndividual)t).getSurname().length() == 0)
 			throw new ClientAppServicesException("Apellido no valido");
-		if(DNI.length() != 9)
+		if(dni.length() != 9)
 			throw new ClientAppServicesException("DNI no valido");
-		if ((TLF.length() != 9))//||(TLF.indexOf("9") == -1)||(TLF.indexOf("6") == -1))
+		if ((phone.length() != 9))//||(TLF.indexOf("9") == -1)||(TLF.indexOf("6") == -1))
 			throw new ClientAppServicesException("Telefono no valido");
-		if((TJC.length() < 13)||(TJC.length() > 16))
+		if((creditCard.length() < 13)||(creditCard.length() > 16))
 			throw new ClientAppServicesException("Tarjeta de credito no valida");
 		if(((ClientTransferIndividual)t).getAddress().length() == 0)
 			throw new ClientAppServicesException("Domicilio no valido");
@@ -88,87 +152,19 @@ public class ClientAppServicesImp implements ClientAppServices {
 
 	private void checkDataCompany(ClientTransfer t) throws ClientAppServicesException {
 		
-		String TLF = ((ClientTransferCompany)t).getPhone();
-		String TJC = ((ClientTransferCompany)t).getCreditCard();
+		String phone = ((ClientTransferCompany)t).getPhone();
+		String creditCard = ((ClientTransferCompany)t).getCreditCard();
 		
 		if(((ClientTransferCompany)t).getCompany().length() == 0)
 			throw new ClientAppServicesException("Nombre no valido");
 		if(((ClientTransferCompany)t).getAddress().length() == 0)
 			throw new ClientAppServicesException("Domicilio no valido");
-		if ((TLF.length() != 9)||(TLF.indexOf("9") == -1)||(TLF.indexOf("6") == -1))
+		if ((phone.length() != 9)||(phone.indexOf("9") == -1)||(phone.indexOf("6") == -1))
 			throw new ClientAppServicesException("Telefono no valido");
-		if((TJC.length() < 13)||(TJC.length() > 16))
+		if((creditCard.length() < 13)||(creditCard.length() > 16))
 			throw new ClientAppServicesException("Tarjeta de credito no valida");
 		if(((ClientTransferCompany)t).getCIF().length() != 9)
 			throw new ClientAppServicesException("Domicilio no valido");
-	}
-
-	@Override
-	public void delClient(int id) throws ClientAppServicesException {
-		DAOFactory fac = DAOFactory.getInstance();
-		ClientDAO dao = fac.getClientDAO();
-		
-		try {
-			//el usuario tiene que existir y no puede tener reservas sin confirmar(no pagadas)
-			if (dao.searchClient(id)){
-				if(dao.allBooksConfirmed(id)){
-					dao.deleteClient(id);
-				}
-				else{
-					throw new ClientAppServicesException("El usuario a eliminar tiene reservas pendientes de confirmar");
-				}
-			}else{
-				throw new ClientAppServicesException("El usuario a eliminar no existe");
-			}
-		} catch (ClientIntegrationException e) {
-			e.getMessage();
-			throw new ClientAppServicesException("Problema al eliminar cliente");
-		}
-		
-	}
-
-	@Override
-	public Vector<ClientTransfer> listClient() throws ClientAppServicesException {
-		DAOFactory fac = DAOFactory.getInstance();
-		ClientDAO dao = fac.getClientDAO();
-		try {
-			return dao.listClient();
-		} catch (ClientIntegrationException e) {
-			e.getMessage();
-			throw new ClientAppServicesException(e.getMessage());
-		}
-
-	}
-
-	@Override
-	public void modClient(ClientTransfer t) throws ClientAppServicesException {
-
-		DAOFactory fac = DAOFactory.getInstance();
-		ClientDAO dao = fac.getClientDAO();
-		
-		if(t instanceof ClientTransferIndividual ) {
-			try {
-				if(dao.searchClient(t.getID())){
-					dao.updateClientIndividual(t);
-				}else{
-					throw new ClientAppServicesException("Cliente no encontrado");
-				}
-			} catch (ClientIntegrationException e) {
-				e.printStackTrace();
-			}
-		}
-		else{
-			try {
-				if(dao.searchClient(t.getID())){
-					dao.updateClientCompany(t);
-				}else{
-					throw new ClientAppServicesException("Cliente no encontrado");
-				}
-			} catch (ClientIntegrationException e) {
-				e.printStackTrace();
-			}
-		}
-		
 	}
 
 }
